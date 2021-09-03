7b9f743b7798f3ad721a591683660f7a4bfcf7bf
amar-preet-s <ssasbby629@gmail.com>
2021-09-03 19:49:08 +0530

    update-2

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+let playlist = {"Sidhu-moosewala":{
+                                    "pic":"https://www.uptobrain.com/wp-content/uploads/2020/09/sidhu-moose-wala.jpg",
+                                    "name":"Sidhu Moosewala",
+                                    "songs":[{"name":"295",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":false,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"},
+                                              {"name":"amar",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"},
+                                              
+                                            ]
+                                  },
+                "Amrinder-gill":{
+                                    "pic":"https://5abnow.com/wp-content/uploads/2018/02/Amrinder-gill-latest-photo-2018.jpg",
+                                    "name":"Amrinder Gill",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                "Diljit-dosanjh":{
+                                    "pic":"https://i.pinimg.com/originals/3a/ed/07/3aed07fa8771691ff0670626972dee8e.jpg",
+                                    "name":"Diljit Dosanjh",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                "Ammy-virk":{
+                                    "pic":"https://th.bing.com/th/id/OIP.nEqnAE7i4GNiy2qPiuTtZQAAAA?pid=ImgDet&w=350&h=350&rs=1",
+                                    "name":"Ammy Virk",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                "Karan-aujla":{
+                                    "pic":"https://m.media-amazon.com/images/I/912VIfA5pFL._SS500_.jpg",
+                                    "name":"Karan Aujla",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                 "Atif-aslam":{
+                                    "pic":"https://lh3.googleusercontent.com/proxy/6PY12ClGB3HmICxBG-zzeQDVKer7vEwHcGDHFphFAHE9gmGA859DaZGmgiKQOLFluU6Sm7K3ZMK6GMuquqwScB1Y9ZMdhh6KjzSw-wpYkK5wpRCcQhdnA9hShFc",
+                                    "name":"Atif Aslam",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                 "Arijit-singh":{
+                                    "pic":"https://cdn.trell.co/w=640,h=640,fit=scale-down/format=webp,quality=50/user-images/images/orig/ekWbe7k7PI0o4naU8VWYuDyU2aSFysmY.jpg",
+                                    "name":"Arijit Singh",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                 "Jubin-nautyal":{
+                                    "pic":"https://i2.cinestaan.com/image-bank/1500-1500/155001-156000/155864.jpg",
+                                    "name":"Jubin Nautyal",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                 "Sonu-nigam":{
+                                    "pic":"https://st1.bollywoodlife.com/wp-content/uploads/2021/06/Sonu-Nigam.jpg",
+                                    "name":"Sonu Nigam",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":true,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                 "Darshan-raval":{
+                                    "pic":"https://i.scdn.co/image/ab6761610000e5eb61e352af80cf3746c61e10a4",
+                                    "name":"Darshan Raval",
+                                    "songs":[{"name":"vibe",
+                                              "time":"03:00",
+                                              "last-date-added":"29-08-2021",
+                                              "liked":false,
+                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
+                                            ]
+                                  },
+                                  
+            }
+
 
 $(".search-bar").focus(function(){
     $(this).text("");
@@ -22,3 +131,65 @@ $(".song-player-container").scroll(function () {
   }
 
 });
+
+// adding songs in web we get ["language","artist"] in data
+function addSongs(data){
+    // console.log(playlist[data[0]][data[1]]["songs"][0]);
+    let song_data = playlist[data]["songs"];
+    for(let i = 0;i<song_data.length;i++){
+      let song_row = `  <div class="songs">
+                          <div class="song-number-col song-no-${i}">${i+1}</div>
+                          <div class="song-title-col song-title">${song_data[i]["name"]}</div>
+                          <div class="last-date-added-col song-date">${song_data[i]["last-date-added"]}</div>
+                          <div class="time-col song-schedule">${song_data[i]["time"]}</div>
+                          <div class="play-col song-play"><div class="material-icons playlist-play-icon">play_arrow</div></div>
+                          <div class="favorite-col ${song_data[i]["liked"]?"liked":""} song-like"><span class="material-icons">favorite</span></div>
+                        </div>`;
+      $(".songs-list").append(song_row);
+    }
+}
+
+// addSongs("sidhu-moose-wala");
+
+function addPlaylist(){
+  $(".player-container").append(`<div class="playlist-queues">
+                                    <div class="playlist-heading">Playlists</div>
+                                    <div class="playlists-section">
+                                  </div>`);
+  $(".playlists-section").html("");
+  for(let i of Object.keys(playlist)){
+    let pList = `<div class="playlist-container ${i}">
+                      <div class="playlist-img"><img class="picture" src="${playlist[`${i}`]["pic"]}"/></div>
+                      <div class="playlist-play-icon-container"><div class="material-icons playlist-play-icon">play_arrow</div></div>
+                      <div class="playlist-content">${playlist[`${i}`]["name"]}</div>
+                  </div>`;
+    $(".playlists-section").append(pList);
+  }
+}
+
+addPlaylist();
+
+
+$(".playlist-container").click(function(e){
+  $('.playlist-queues').remove();
+  $(".player-container").append(`<div class="song-player-container">
+                                    <div class="player">
+                                        <div class="cover-page">
+                                            <div class="video-container"></div>
+                                              <div class="album-photo-container"></div>
+                                        </div>
+                                        <div class="songs-list">
+                                            <div class="song-head-bar">
+                                                <div class="song-number-col">#</div>
+                                                <div class="song-title-col">Title</div>
+                                                <div class="last-date-added-col">Date</div>
+                                                <div class="time-col"><span class="material-icons">schedule</span></div>
+                                                <div class="play-col">Play</div>
+                                                <div class="favorite-col header"><span class="material-icons">favorite</span></div>
+                                            </div>
+                                        </div> 
+                                    </div>
+                                  </div>`);
+  addSongs(`${$(e.currentTarget).attr("class").split(" ")[1]}`);
+  
+})
