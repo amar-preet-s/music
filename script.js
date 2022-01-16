2b8b67b08db3cb0b727ffe01db393d476c8b5675
amarpreet <ssabby629@gmail.com>
2022-01-16 22:19:08 +0530

    final-commit

--- script.js
+++ script.js
@@ -1,113 +1,225 @@
-let playlist = {"Sidhu-moosewala":{
-                                    "pic":"https://www.uptobrain.com/wp-content/uploads/2020/09/sidhu-moose-wala.jpg",
-                                    "name":"Sidhu Moosewala",
-                                    "songs":[{"name":"295",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":false,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"},
-                                              {"name":"amar",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"},
-                                              
-                                            ]
-                                  },
-                "Amrinder-gill":{
-                                    "pic":"https://5abnow.com/wp-content/uploads/2018/02/Amrinder-gill-latest-photo-2018.jpg",
-                                    "name":"Amrinder Gill",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                "Diljit-dosanjh":{
-                                    "pic":"https://i.pinimg.com/originals/3a/ed/07/3aed07fa8771691ff0670626972dee8e.jpg",
-                                    "name":"Diljit Dosanjh",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                "Ammy-virk":{
-                                    "pic":"https://th.bing.com/th/id/OIP.nEqnAE7i4GNiy2qPiuTtZQAAAA?pid=ImgDet&w=350&h=350&rs=1",
-                                    "name":"Ammy Virk",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                "Karan-aujla":{
-                                    "pic":"https://m.media-amazon.com/images/I/912VIfA5pFL._SS500_.jpg",
-                                    "name":"Karan Aujla",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                 "Atif-aslam":{
-                                    "pic":"https://lh3.googleusercontent.com/proxy/6PY12ClGB3HmICxBG-zzeQDVKer7vEwHcGDHFphFAHE9gmGA859DaZGmgiKQOLFluU6Sm7K3ZMK6GMuquqwScB1Y9ZMdhh6KjzSw-wpYkK5wpRCcQhdnA9hShFc",
-                                    "name":"Atif Aslam",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                 "Arijit-singh":{
-                                    "pic":"https://cdn.trell.co/w=640,h=640,fit=scale-down/format=webp,quality=50/user-images/images/orig/ekWbe7k7PI0o4naU8VWYuDyU2aSFysmY.jpg",
-                                    "name":"Arijit Singh",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                 "Jubin-nautyal":{
-                                    "pic":"https://i2.cinestaan.com/image-bank/1500-1500/155001-156000/155864.jpg",
-                                    "name":"Jubin Nautyal",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                 "Sonu-nigam":{
-                                    "pic":"https://st1.bollywoodlife.com/wp-content/uploads/2021/06/Sonu-Nigam.jpg",
-                                    "name":"Sonu Nigam",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":true,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                 "Darshan-raval":{
-                                    "pic":"https://i.scdn.co/image/ab6761610000e5eb61e352af80cf3746c61e10a4",
-                                    "name":"Darshan Raval",
-                                    "songs":[{"name":"vibe",
-                                              "time":"03:00",
-                                              "last-date-added":"29-08-2021",
-                                              "liked":false,
-                                              "src":"https://s48.ve.vc/data/48/50413/298090/295%20-%20Sidhu%20Moose%20Wala.mp3"}
-                                            ]
-                                  },
-                                  
-            }
+
+let songs = [{"name":"Fade away",
+              "time":"03:32",
+              "last-date-added":"29-08-2021",
+              "playing":true ,
+              "Genre":"sad" ,
+              "src":"Songs/alan-walker-faded.mp3"},
+              {"name":"Closer",
+              "time":"04:07",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"happy" ,
+              "src":"Songs/Closer - The Chainsmokers.mp3"},
+              {"name":"Despacito",
+              "time":"03:48",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"party" ,
+              "src":"Songs/despacito-ft-daddy-yankee.mp3"},
+              {"name":"Dusk Till Dawn",
+              "time":"04:27",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"sad" ,
+              "src":"Songs/Dusk Till Dawn.mp3"},
+              {"name":"I Hate You",
+              "time":"04:29",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"sad" ,
+              "src":"Songs/gnash - i hate u, i love u (ft. olivia o brien).mp3"},
+              {"name":"Let me love you",
+              "time":"04:30",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"Love" ,
+              "src":"Songs/let-me-love-you.mp3"},
+              {"name":"Girls like you",
+              "time":"04:30",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"party" ,
+              "src":"Songs/maroon-5-girls-like-you.mp3"},
+              {"name":"Right now",
+              "time":"03:21",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"party" ,
+              "src":"Songs/Nick_Jonas_Robin_Schulz_Right_Now.mp3"},
+              {"name":"perfect",
+              "time":"04:20",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"love" ,
+              "src":"Songs/Perfect.mp3"},
+              {"name":"See you again",
+              "time":"03:49",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"happy" ,
+              "src":"Songs/see-you-again-ft-charlie-puth.mp3"},
+              {"name":"Senorita",
+              "time":"03:17",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"party" ,
+              "src":"Songs/senorita (mp3cut.net) (1).mp3"},
+              {"name":"Taki Taki",
+              "time":"03:32",
+              "last-date-added":"29-08-2021",
+              "playing":false ,
+              "Genre":"party" ,
+              "src":"Songs/taki-taki-mp3-free-download.mp3"},
+]; 
+
+let rewind = document.querySelector(".rewind");
+let playpausebtn = document.querySelector(".play-pause");
+let forward = document.querySelector(".forward");
+let timer = document.querySelector(".timer");
+let volume = document.querySelector(".volume");
+
+let startmin = document.querySelector(".minute");
+let startsec = document.querySelector(".second");
+let endmin = document.querySelector(".end-minute");
+let endsec = document.querySelector(".end-second");
+
+let volumeBtn = document.querySelector(".volume");
+let volumeSliderContainer = document.querySelector(".volumeSliderContainer");
+let volumeSlider = document.getElementById("volumeSlider");
+
+let trackindex = 0;
+let current;
+let songplaying = false;
+
+let source = document.querySelector("#source");
+let player = document.querySelector("#player");
+
+forward.addEventListener("click", function () {
+  songplaying = false;
+  playpausebtn.innerText = "play_circle_filled";
+  player.load();
+  setTimeout(() => {
+    playpausebtn.click();
+  }, 300);
+  document.querySelector(".playing").classList.remove("playing");
+  nextTrack();
+});
+
+rewind.addEventListener("click", function () {
+  console.log("you must have clicked the rewind");
+ 
+  songplaying = false;
+  playpausebtn.innerText = "play_circle_filled";
+  player.load();
+  setTimeout(() => {
+    playpausebtn.click();
+  }, 300);
+  document.querySelector(".playing").classList.remove("playing");
+  prevTrack();
+});
+function nextTrack() {
+  songs[trackindex]["playing"] = false;
+  if (trackindex < songs.length - 1) {
+    trackindex += 1;
+  } else trackindex = 0;
+  source.src = songs[trackindex]["src"];
+  var nexttarget = document.getElementsByClassName(`song-no-${trackindex}`);
+  console.log(nexttarget);
+  songs[trackindex]["playing"] = true;
+  nexttarget[0].classList.add("playing");
+}
+
+function prevTrack() {
+  console.log("prevtrack " + typeof trackindex);
+  if (trackindex > 0) trackindex -= 1;
+  else trackindex = songs.length - 1;
+  source.src = songs[trackindex]["src"];
+  console.log(trackindex);
+  var nexttarget = document.getElementsByClassName(`song-no-${trackindex}`);
+  nexttarget[0].classList.add("playing");
+}
 
 
+volumeSlider.addEventListener("change", function (e) {
+  const volume = e.target.value;
+  volumeSlider.value = volume;
+  player.volume = volume;
+});
+
+
+playpausebtn.addEventListener("click", function () {
+  if (!songplaying) {
+    songplaying = true;
+    playpausebtn.innerText = "pause_circle_outline";
+    player.play();
+    console.log("play in playpausebtn");
+  } else {
+    songplaying = false;
+    playpausebtn.innerText = "play_circle_filled";
+    player.pause();
+    console.log("pause in playpausebtn");
+  }
+});
+
+let timerInterval;
+let second = 0;
+let minute = 0;
+
+let progbar = document.querySelector(".progress-bar-container");
+
+progbar.addEventListener("click", function (e) {
+  player.currentTime = (e.offsetX / progbar.offsetWidth) * player.duration;
+});
+
+function updateProgressBar(e) {
+  //updating progress bar
+
+  let { currentTime, duration } = e.srcElement;
+  timer.style.width = Math.floor((currentTime / duration) * 100) + "%";
+
+  // Calculate display for duration
+  let durationMinutes = Math.floor(duration / 60);
+  let durationSeconds = Math.floor(duration % 60);
+  if (durationSeconds < 10) {
+    durationSeconds = `0${durationSeconds}`;
+  }
+
+  // Delay switching duration Element to avoid NaN
+  if (durationSeconds) {
+    if (durationMinutes < 10) {
+      durationMinutes = `0${durationMinutes}`;
+    }
+    endmin.innerText = durationMinutes;
+    endsec.innerText = durationSeconds;
+  }
+
+  // Calculate display for currentTime
+  let currentMinutes = Math.floor(currentTime / 60);
+  let currentSeconds = Math.floor(currentTime % 60);
+
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  if (currentSeconds < 10) {
+    currentSeconds = `0${currentSeconds}`;
+  }
+
+  startmin.innerText = currentMinutes;
+  startsec.innerText = currentSeconds;
+}
+player.addEventListener("timeupdate", updateProgressBar);
+
+function addactive(e) {
+  current = document.querySelector(".playing");
+  console.log(current);
+  if (current) {current.classList.remove("playing");
+    songs[trackindex]["playing"] = false;}
+
+  e.currentTarget.classList.add("playing");
+}
+
+// ----
 $(".search-bar").focus(function(){
     $(this).text("");
     $(this).css({"border":"1px solid silver","border-radius":"4px"});
@@ -133,24 +245,44 @@ $(".song-player-container").scroll(function () {
 });
 
 // adding songs in web we get ["language","artist"] in data
-function addSongs(data){
-    // console.log(playlist[data[0]][data[1]]["songs"][0]);
-    let song_data = playlist[data]["songs"];
+function addSongs(song_data){
+
     for(let i = 0;i<song_data.length;i++){
-      let song_row = `  <div class="songs">
-                          <div class="song-number-col song-no-${i}">${i+1}</div>
+      let song_row = `  <div class="songs song-no-${i} ${song_data[i]["playing"]?"playing":""}">
+                          <div class="song-number-col">${i+1}</div>
                           <div class="song-title-col song-title">${song_data[i]["name"]}</div>
                           <div class="last-date-added-col song-date">${song_data[i]["last-date-added"]}</div>
                           <div class="time-col song-schedule">${song_data[i]["time"]}</div>
-                          <div class="play-col song-play"><div class="material-icons playlist-play-icon">play_arrow</div></div>
-                          <div class="favorite-col ${song_data[i]["liked"]?"liked":""} song-like"><span class="material-icons">favorite</span></div>
+                          <div class="gen-col song-play">${song_data[i]["Genre"]}</div>
+                          
                         </div>`;
       $(".songs-list").append(song_row);
     }
+  addEventListenerSongs();
+
 }
 
-// addSongs("sidhu-moose-wala");
 
+
+function addEventListenerSongs(){
+  document.querySelectorAll(".songs").forEach((item) => {
+    item.addEventListener("click", function (e) {
+      console.log("select songs");
+      addactive(e);
+      let i = e.currentTarget.classList[1].split("-")[2]; //getting id of current song to get its url
+      trackindex = parseInt(i);
+      songs[trackindex]["playing"] = true;
+      source.src = songs[i]["src"];
+      songplaying = false;
+      playpausebtn.innerText = "play_circle_filled";
+      player.load();
+      setTimeout(() => {
+        playpausebtn.click();
+      }, 100);
+      updateProgressBar(e);
+    });
+  });
+}
 function addPlaylist(){
   $(".player-container").append(`<div class="playlist-queues">
                                     <div class="playlist-heading">Playlists</div>
@@ -165,31 +297,8 @@ function addPlaylist(){
                   </div>`;
     $(".playlists-section").append(pList);
   }
+
 }
 
-addPlaylist();
 
-
-$(".playlist-container").click(function(e){
-  $('.playlist-queues').remove();
-  $(".player-container").append(`<div class="song-player-container">
-                                    <div class="player">
-                                        <div class="cover-page">
-                                            <div class="video-container"></div>
-                                              <div class="album-photo-container"></div>
-                                        </div>
-                                        <div class="songs-list">
-                                            <div class="song-head-bar">
-                                                <div class="song-number-col">#</div>
-                                                <div class="song-title-col">Title</div>
-                                                <div class="last-date-added-col">Date</div>
-                                                <div class="time-col"><span class="material-icons">schedule</span></div>
-                                                <div class="play-col">Play</div>
-                                                <div class="favorite-col header"><span class="material-icons">favorite</span></div>
-                                            </div>
-                                        </div> 
-                                    </div>
-                                  </div>`);
-  addSongs(`${$(e.currentTarget).attr("class").split(" ")[1]}`);
-  
-})
+addSongs(songs);
